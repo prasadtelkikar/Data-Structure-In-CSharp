7513533f204af17e4afae703b2bb96da0dfe1b3e
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-01 23:26:19 +0530

    Added LL delete functionality.

--- Linked-List/SinglyLinkedList.cs
+++ Linked-List/SinglyLinkedList.cs
@@ -47,10 +47,102 @@ namespace Linked_List
                 InsertAtEnd(item);
         }
 
-        public void InsertAtLocation(int index)
+        //0 based indexing
+        public void InsertAtLocation(int data, int index)
         {
+            var newNode = CreateNewNode(data);
+            var currentNode = head;
+            Node previousNode = null;
+            var currentIndex = 0;
+            if(index == 0)
+            {
+                newNode.next = currentNode;
+                head = newNode;
+                this.length++;
+            }
+            else
+            {
+                //Traverse the list until the position where we want to insert
+                while(currentNode != null && currentIndex < index)
+                {
+                    currentIndex++;
+                    previousNode = currentNode;
+                    currentNode = currentNode.next;
+                }
+
+                //Insert new node
+                previousNode.next = newNode;
+                newNode.next = currentNode;
+                this.length++;
+            }
 
         }
+
+        private void DeleteAtBeginning()
+        {
+            var currentNode = head;
+            if (head == null)
+                Console.WriteLine("Linked List is already empty, can't delete node anymore");
+            else
+            {
+                head = currentNode.next;
+                currentNode.next = null;
+                this.length--;
+            }
+        }
+
+        private void DeleteAtEnd()
+        {
+            var currentNode = head;
+            Node previousNode = null;
+            if (currentNode == null)
+                Console.WriteLine("Linked List is already empty, can't delete node anymore");
+            else
+            {
+                while(currentNode.next != null)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.next;
+                }
+                previousNode.next = null;
+                currentNode = null;
+                this.length--;
+            }
+
+        }
+
+        //0 based indexing
+        private void DeleteAtLocation(int index)
+        {
+            var currentNode = head;
+            Node previousNode = null;
+            var currentIndex = 0;
+
+            if (currentNode == null)
+            {
+                Console.WriteLine("Linked list is already empty, can't delete node anymore");
+                return;
+            }
+            else
+            {
+                while (currentNode != null && currentIndex < index)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.next;
+                    currentIndex++;
+                }
+
+                if (currentNode == null)
+                    Console.WriteLine("Wrong index entered, can't delete");
+                else
+                {
+                    previousNode.next = currentNode.next;
+                    currentNode = null;
+                    this.length--;
+                }
+            }
+        }
+
         private void Display()
         {
             var currentNode = head;
@@ -63,7 +155,7 @@ namespace Linked_List
                     if(currentNode.next != null)
                         Console.Write($"{currentNode.data} -> ");
                     else
-                        Console.Write($"{currentNode.data} -> NULL");
+                        Console.WriteLine($"{currentNode.data} -> NULL");
                     currentNode = currentNode.next;
                 }
             }
@@ -91,13 +183,25 @@ namespace Linked_List
         {
             var beginning_data = new List<int>() { 6, 5, 4, 3, 2, 1};
             var end_data = new List<int>() { 10, 20, 30, 40, 50, 60 };
+
             SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+
+            //Insert
             singlyLinkedList.InsertMultiNodeAtBeginning(beginning_data);
             singlyLinkedList.InsertMultiNodeAtEnd(end_data);
+            singlyLinkedList.InsertAtLocation(1000, 6);
+
+            //Delete
+            singlyLinkedList.DeleteAtBeginning();
+            singlyLinkedList.DeleteAtEnd();
+            singlyLinkedList.DeleteAtLocation(5);
+
+            //Utilities
             singlyLinkedList.Display();
-            Console.WriteLine($"Length of Given LinkedList = {singlyLinkedList.GetLength()}");
+            Console.WriteLine($"{Environment.NewLine}Length of Given LinkedList = {singlyLinkedList.GetLength()}");
             Console.WriteLine($"Length of Given LinkedList = {singlyLinkedList.GetLengthByNode()}");
         }
+
         private class Node
         {
             public int data = 0;
