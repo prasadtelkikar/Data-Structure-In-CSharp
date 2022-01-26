9c518dc21bb0aae7c44a4687e673218de642df98
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-01-26 22:52:54 +0530

    Solved reverse linked list problem

---
+++ Linked-List-Problems/ReverseLinkedList-NonRecursive.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Problems
+{
+    public class ReverseLinkedList_NonRecursive
+    {
+        private Node head = null;
+        private int length = 0;
+
+        private void InsertAtBeginning(int data)
+        {
+            var newNode = CreateNewNode(data);
+            if(head == null)
+            {
+                head = newNode;
+                this.length++;
+                return;
+            }
+            newNode.NextData = head;
+            head=newNode;
+            this.length++;
+        }
+
+        private void InsertMultiple(IList<int> elements)
+        {
+            foreach (var element in elements)
+                InsertAtBeginning(element);
+        }
+
+        private void Display(Node head)
+        {
+            for (Node currentNode = head; currentNode != null; currentNode=currentNode.NextData)
+            {
+                if (currentNode.NextData == null)
+                    Console.WriteLine($"{currentNode.Data} -> null");
+                else
+                    Console.Write($"{currentNode.Data} -> ");
+            }
+        }
+
+        private Node ReverseLinkedList()
+        {
+            Node currentNode = this.head;
+            Node previousNode = null;
+
+            while(currentNode != null)
+            {
+                var nextOfCurrent = currentNode.NextData;
+                currentNode.NextData = previousNode;
+                previousNode = currentNode;
+                currentNode = nextOfCurrent;
+            }
+            return previousNode;
+        }
+
+        public static void Main(string[] args)
+        {
+            var elements = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+            ReverseLinkedList_NonRecursive reverseObj = new ReverseLinkedList_NonRecursive();
+
+            reverseObj.InsertMultiple(elements);
+            Console.WriteLine("Before Reverse");
+            reverseObj.Display(reverseObj.head);
+
+            var reversedList = reverseObj.ReverseLinkedList();
+            Console.WriteLine("After Reverse");
+            reverseObj.Display(reversedList);
+        }
+        private Node CreateNewNode(int data) => new Node(data);
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node NextData { get; set; }
+
+            public Node(int data) { Data = data; }
+        }
+    }
+}
---
+++ Linked-List-Problems/ReverseLinkedList-Recursion.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Problems
+{
+    public class ReverseLinkedList_Recursion
+    {
+        private Node head = null;
+        private int length = 0;
+
+        private void InsertAtBeginning(int data)
+        {
+            var newNode = CreateNewNode(data);
+            if(head != null)
+                newNode.NextNode = head;
+            head = newNode;
+            this.length++;
+        }
+
+        private void InsertMultiple(IList<int> elements)
+        {
+            foreach (var element in elements)
+                InsertAtBeginning(element);
+        }
+
+        private void Display(Node head)
+        {
+            for (Node currentNode = head; currentNode != null; currentNode = currentNode.NextNode)
+            {
+                if (currentNode.NextNode == null)
+                    Console.WriteLine($"{currentNode.Data} -> null");
+                else
+                    Console.Write($"{currentNode.Data} -> ");
+            }
+        }
+
+        //TODO: Bug in the recurssion. Need to fix
+        private Node ReveseLinkedListRecursively(Node head)
+        {
+            if (head == null)
+                return null;
+            else if (head.NextNode == null)
+                return head;
+
+            //Recursively reach to the last node
+            var temp = ReveseLinkedListRecursively(head.NextNode);
+
+            //Reverse logic
+            var lastNode = temp;
+            lastNode.NextNode = head;
+            head.NextNode = null;
+
+            return temp;
+        }
+
+        private Node CreateNewNode(int data) => new Node(data);
+
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            ReverseLinkedList_Recursion reverseObj = new ReverseLinkedList_Recursion();
+            reverseObj.InsertMultiple(list);
+
+            Console.WriteLine("Before Reverse");
+            reverseObj.Display(reverseObj.head);
+
+            var reverseList = reverseObj.ReveseLinkedListRecursively(reverseObj.head);
+
+            Console.WriteLine("After Reverse");
+            reverseObj.Display(reverseObj.head);
+        }
+
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node NextNode { get; set; }
+            public Node(int data) => this.Data = data;
+        }
+    }
+}
