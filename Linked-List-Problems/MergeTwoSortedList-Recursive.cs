1fcfc58f8398a23d9b6679b42d2c778b9f6e90a6
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-02-18 23:49:30 +0530

    Skeleton for Merge sorted list problem

---
+++ Linked-List-Problems/MergeTwoSortedList-Recursive.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Problems
+{
+    public class MergeTwoSortedList_Recursive
+    {
+        private Node firstHead = null;
+        private Node secondHead = null;
+
+        private void InsertMultiple(IList<int> elements, bool addToFirst = true)
+        {
+            foreach (var element in elements)
+            {
+                var newNode = CreateNewNode(element);
+                if (addToFirst)
+                {
+                    if (firstHead != null)
+                        newNode.NextNode = firstHead;
+                    firstHead = newNode;
+                }
+                else
+                {
+                    if (secondHead != null)
+                        newNode.NextNode = secondHead;
+                    secondHead = newNode;
+                }
+            }
+        }
+
+        private void Display(Node head)
+        {
+            for (var currentNode = head; currentNode != null; currentNode=currentNode.NextNode)
+                Console.Write(currentNode.NextNode != null ? $"{currentNode.Data} -> " : $"{currentNode.Data} -> NULL{Environment.NewLine}");
+        }
+
+        private Node MergeList(Node firstListNode, Node secondListNode)
+        {
+            //TODO: Add recursive technique to merge two sorted lists
+        }
+
+        private Node CreateNewNode(int data) => new Node(data);
+        public static void Main(string[] args)
+        {
+
+        }
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node NextNode { get; set; }
+            public Node(int data) => this.Data = data;
+        }
+
+    }
+}
