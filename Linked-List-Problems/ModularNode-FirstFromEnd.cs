80dfd56ef4c6d13970b4193ee73a7cd574a6ef07
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-03-20 23:20:00 +0530

    Solved modulo frist problem from linked list

---
+++ Linked-List-Problems/ModularNode-FirstFromEnd.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Problems
+{
+
+    public class ModularNode_FirstFromEnd
+    {
+        private Node head = null;
+        private void InsertAtBeginning(int data)
+        {
+            var newNode = CreateNewNode(data);
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            newNode.NextNode = head;
+            head = newNode;
+        }
+
+        private void InsertMultiple(IList<int> elements)
+        {
+            foreach (var element in elements)
+                InsertAtBeginning(element);
+        }
+
+        //This function will break linked list in modulo condition, and will return first element from last list
+        private Node ModuleNodeFirstFromEnd(int moduloValue)
+        {
+            //1 based index
+            int index = 1;
+            Node moduloListHead = head;
+            for(Node currentNode = head; currentNode.NextNode != null; currentNode = currentNode.NextNode)
+            {
+                if(index % moduloValue == 0)
+                    moduloListHead = currentNode;
+                index++;
+            }
+            return moduloListHead.NextNode;
+        }
+        private void Display()
+        {
+            for (var currentNode = head; currentNode != null; currentNode=currentNode.NextNode)
+                Console.Write(currentNode.NextNode != null ? $"{currentNode.Data} -> " : $"{currentNode.Data} -> NULL{Environment.NewLine}");
+        }
+        private Node CreateNewNode(int data) => new Node(data); 
+        
+        public static void Main(string[] args)
+        {
+            var elements = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            int moduloValue = 3;
+            
+            ModularNode_FirstFromEnd moduloObj = new ModularNode_FirstFromEnd();
+            moduloObj.InsertMultiple(elements);
+
+            moduloObj.Display();
+
+            var result = moduloObj.ModuleNodeFirstFromEnd(moduloValue);
+            Console.WriteLine(result.Data);
+
+        }
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node NextNode { get; set; }
+            public Node(int data) => this.Data = data;
+        }
+    }
+}
