f469da76eaa853044df84b54cd7d71d3016db049
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-04-08 21:47:42 +0530

    Solved GetMin and symbol balancing problem

---
+++ Stack-Problems/GetMinimumByStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Stack_Problems
+{
+    public class GetMinimumByStack
+    {
+        private Node top = null;
+        private Node minTop = null;
+
+        private void Push(int data)
+        {
+            var newNode = CreateNewNode(data);
+            if (top == null)
+            {
+                top = newNode;
+                minTop = newNode;
+                return;
+            }
+            if(minTop.Data < data)
+            {
+                var min = CreateNewNode(minTop.Data);
+                min.NextData = minTop;
+
+                newNode.NextData = top;
+
+                top = newNode;
+                minTop = min; 
+            }
+            else
+            {
+                newNode.NextData = top;
+                top = newNode;
+
+                var minNewNode = CreateNewNode(data);
+                minNewNode.NextData = minTop;
+                minTop = minNewNode;
+            }
+        }
+
+        private int GetMinimum() => minTop.Data;
+        private Node CreateNewNode(int data) => new Node(data);
+
+        public static void Main(string[] args)
+        {
+            GetMinimumByStack getMin = new GetMinimumByStack();
+            getMin.Push(2);
+            getMin.Push(6);
+            getMin.Push(4);
+            getMin.Push(1);
+            getMin.Push(5);
+            Console.WriteLine($"Min From input stack = {getMin.GetMinimum()}");
+        }
+        private class Node
+        {
+            public int Data { get; set; }
+            public Node NextData { get; set; }
+            public Node(int data) { this.Data = data; this.NextData = null; }
+        }
+    }
+}
--- Stack-Problems/SymbolBalancing.cs
+++ Stack-Problems/SymbolBalancing.cs
@@ -8,5 +8,73 @@ namespace Stack_Problems
 {
     public class SymbolBalancing
     {
+        private Node top = null;
+        private void Push(char data)
+        {
+            var newNode = CreateNewNode(data);
+            if(top == null)
+            {
+                top = newNode;
+                return;
+            }
+            newNode.NextNode = top;
+            top = newNode;
+        }
+
+        public void PushMultiple(string inputStr)
+        {
+            foreach (char item in inputStr)
+                Push(item);
+        }
+
+        private bool IsStackEmpty() => top == null;
+
+        private char Pop()
+        {
+            if (IsStackEmpty())
+                return '\0';
+            var temp = top.Data;
+            top = top.NextNode;
+            return temp;
+        }
+
+        private bool IsEquationBalanced(string equation)
+        {
+            var openingBrackets = "([{";
+            var closingBrackets = ")]}";
+            foreach (char eq in equation)
+            {
+                if(openingBrackets.Contains(eq))
+                    Push(eq);
+                else if(closingBrackets.Contains(eq))
+                {
+                    if(top==null)
+                        return false;
+                    var openingElementIndex = openingBrackets.IndexOf(Pop());
+                    var closingElementIndex = closingBrackets.IndexOf(eq);
+                    if (openingElementIndex != closingElementIndex)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private Node CreateNewNode(char data) => new Node(data);
+
+        public static void Main(string[] args)
+        {
+            SymbolBalancing symbol = new SymbolBalancing();
+            Console.WriteLine("Enter an equation");
+            var equation = Console.ReadLine();
+            var isBalanced = symbol.IsEquationBalanced(equation);
+            Console.WriteLine(isBalanced ? "Equation is Balanced" : "Equation is Not balanced");
+            Console.ReadKey();
+        }
+        private class Node
+        {
+            public char Data { get; set; }
+            public Node NextNode { get; set; }
+            public Node(char data) { this.Data = data; this.NextNode = null; }
+        }
     }
 }
