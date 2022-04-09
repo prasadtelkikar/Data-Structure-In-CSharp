5d455ea16f2594bc07064e23175cb3d7b5b9a5f0
Prasad L Telkikar <telkikar.prasad@gmail.com>
2022-04-09 22:45:46 +0530

    Solved palindrome using stack

---
+++ Stack-Problems/Palindrome-Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Problems
+{
+    //For linked list apparoach, Check  Linked_List_Problems -> IsLinkedListPalindrome
+    public class Palindrome_Stack
+    {
+        private Node top = null;
+        private void Push(char data)
+        {
+            var newNode = CreateNewNode(data);
+            if(top == null)
+            {
+                top = newNode;
+                return;
+            }
+            newNode.NextNode = top;
+            top = newNode;
+        }
+
+        private char Pop()
+        {
+            var topChar = top.Data;
+            top = top.NextNode;
+            return topChar;
+        } 
+
+        private bool IsPalindromeCheck(string input)
+        {
+            bool isFirstPart = true;
+            if (!input.Contains('X'))
+                throw new Exception("Invalid input string");
+            foreach(char c in input)
+            {
+                if(c == 'X')
+                    isFirstPart = false;
+                else if(isFirstPart)
+                    Push(c);
+                else if(c !=  Pop())
+                        return false;
+            }
+            return true;
+        }
+
+        private Node CreateNewNode(char data) => new Node(data);
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Palindrome_Stack palindrome = new Palindrome_Stack();
+            bool isPalindrome = palindrome.IsPalindromeCheck(input);
+            Console.WriteLine(isPalindrome ? "Given string is a Palindrome" : "Given string is NOT a palindrome");
+            Console.ReadKey();
+        }
+
+        private class Node
+        {
+            public char Data { get; set; }
+            public Node NextNode { get; set; }
+            public Node(char data) { this.Data = data; this.NextNode = null; }
+        }
+    }
+}
---
+++ Stack-Problems/Palindrome-brute-force.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Problems
+{
+    public class Palindrome_brute_force
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool isPalindrome = FindPalindrome(input);
+            Console.WriteLine(isPalindrome ? $"{input} string is a palindrome" : $"{input} string is NOT a palindrome");
+            Console.ReadKey();
+        }
+
+        private static bool FindPalindrome(string input)
+        {
+            int startIndex = 0;
+            int endIndex = input.Length-1;
+            while(startIndex < endIndex && input[startIndex] == input[endIndex])
+            {
+                startIndex++;
+                endIndex--;
+            }
+            return !(startIndex<endIndex);
+        }
+    }
+}
